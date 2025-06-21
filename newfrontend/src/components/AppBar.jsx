b5227b5158b1e3f0ebf211464c8a5ededda75c0b
Tanisha-Puri <127374875+Tanisha-Puri@users.noreply.github.com>
2025-06-21 13:39:22 +0530

    logo img

--- newfrontend/src/components/AppBar.jsx
+++ newfrontend/src/components/AppBar.jsx
@@ -6,7 +6,7 @@ const AppBar = () => {
   return (
     <header className="app-bar">
       <div className="app-bar-left">
-        <img src="/linkedin_logo.png" alt="LinkedIn Logo" className="logo" />
+        <img src="/LinkedIn_logo.png" alt="LinkedIn Logo" className="logo" />
         <h1 className="app-name">Roadmap Builder and Peer-Pod-Matcher</h1>
       </div>
       <div className="app-bar-right">
